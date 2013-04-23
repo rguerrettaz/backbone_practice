7fff43091ff052c8ca60172ff445ed775a143757
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2013-04-22 18:58:23 -0700

    Downcase email before save in Users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :username
   has_secure_password
+  before_save :downcase_email
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 
+  private
+
+  def downcase_email
+    self.email = self.email.downcase
+  end
 end
