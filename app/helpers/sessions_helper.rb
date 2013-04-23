44dca6a4be04bd28e6e4c8180ea33ad580f956d6
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2013-04-22 20:05:58 -0700

    Add sessions for login and logout

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,28 @@
+module SessionsHelper
+
+  def sign_in(user)
+    session[:id] = user.id
+    self.current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def sign_out
+    self.current_user = nil
+    session.delete(:id)
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    if session[:id]
+      @current_user ||= User.find(session[:id])
+    else
+      @current_user = nil
+    end
+  end
+end
