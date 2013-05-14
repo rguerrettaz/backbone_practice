0e5cdd27a0779775912b8da870b22e85931b938a
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2013-05-14 00:34:01 -0700

    Add MiniProfiler to production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+
+  before_filter :miniprofiler
+
   # Force signout to prevent CSRF attacks
   def handle_unverified_request
     sign_out
@@ -40,4 +43,10 @@ class ApplicationController < ActionController::Base
   end
 
   # helper_method :Score.instance_methods
+
+  private
+
+  def miniprofiler
+    Rack::MiniProfiler.authorize_request
+  end
 end
